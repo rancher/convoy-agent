6b85122673412b2d369edc774de3d1c134945dfb
Craig Jellick <craig@rancher.com>
2015-11-19 22:55:37 -0700

    Packaging and launching refactor
    1. Launch convoy from the convoy-agent binary
    2. Condense down to a single image

--- volume/volume.go
+++ volume/volume.go
@@ -1,33 +1,75 @@
 package volume
 
 import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
+
+	convoyflags "github.com/rancher/convoy/client/flags"
 
 	"github.com/rancher/convoy-agent/cattle"
 )
 
-var rootUuidFileName = "UUID"
+const convoyFlagNamePrefix string = "convoy-"
+const convoyFlagUsagePrefix string = "Passed to convioy. "
+const flagFmt string = "--%s=%s"
 
+var convoyFlagNames = []string{}
+var convoyFlags = map[string]string{}
+var rootUuidFileName = "UUID"
 var Commands = []cli.Command{
 	{
-		Name:   "volume",
-		Usage:  "Start convoy-agent as a volume agent",
-		Action: volumeAgent,
-		Flags: []cli.Flag{
-			cli.StringFlag{
-				Name:  "socket, s",
-				Value: "/var/run/convoy/convoy.sock",
-				Usage: "specify unix domain socket for communicating with convoy server",
-			},
-			cli.StringFlag{
-				Name:   "host-uuid",
-				Usage:  "set the host uuid for the host",
-				EnvVar: "CATTLE_HOST_UUID",
-			},
-		},
+		Name:      "volume",
+		Usage:     "Start convoy-agent as a volume agent",
+		Action:    volumeAgent,
 		ShortName: "v",
 	},
+}
+
+func init() {
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "socket, s",
+			Value: "/var/run/convoy/convoy.sock",
+			Usage: "specify unix domain socket for communicating with convoy server",
+		},
+		cli.StringFlag{
+			Name:   "host-uuid",
+			Usage:  "set the host uuid for the host",
+			EnvVar: "CATTLE_HOST_UUID",
+		},
+	}
+
+	for _, f := range convoyflags.DaemonFlags {
+		// This type switch is annoy, but Name is not exposed on the clie.Flag struct, so we have to cast to the specific types.
+		switch typedF := f.(type) {
+		case cli.StringFlag:
+			typedF.Name = convoyFlagNamePrefix + typedF.Name
+			typedF.Usage = convoyFlagUsagePrefix + typedF.Usage
+			flags = append(flags, typedF)
+			convoyFlags[typedF.Name] = "string"
+			convoyFlagNames = append(convoyFlagNames, typedF.Name)
+		case cli.StringSliceFlag:
+			typedF.Name = convoyFlagNamePrefix + typedF.Name
+			typedF.Usage = convoyFlagUsagePrefix + typedF.Usage
+			flags = append(flags, typedF)
+			convoyFlags[typedF.Name] = "stringslice"
+			convoyFlagNames = append(convoyFlagNames, typedF.Name)
+		case cli.BoolFlag:
+			typedF.Name = convoyFlagNamePrefix + typedF.Name
+			typedF.Usage = convoyFlagUsagePrefix + typedF.Usage
+			flags = append(flags, typedF)
+			convoyFlags[typedF.Name] = "bool"
+			convoyFlagNames = append(convoyFlagNames, typedF.Name)
+		default:
+			logrus.Fatalf("Unknown type. Can't use convoy flag: %#v", f)
+		}
+	}
+	Commands[0].Flags = flags
 }
 
 func volumeAgent(c *cli.Context) {
@@ -42,8 +84,6 @@ func volumeAgent(c *cli.Context) {
 	healthCheckInterval := c.GlobalInt("healthcheck-interval")
 	healthCheckBaseDir := c.GlobalString("healthcheck-basedir")
 
-	controlChan := make(chan bool, 1)
-
 	driver := c.GlobalString("storagepool-driver")
 	if driver == "" {
 		logrus.Fatal("required field storagepool-driver has not been set")
@@ -54,14 +94,56 @@ func volumeAgent(c *cli.Context) {
 		logrus.Fatal("required field host-uuid has not been set")
 	}
 
-	cattleClient, err := cattle.NewCattleClient(cattleUrl, cattleAccessKey, cattleSecretKey)
-	if err != nil {
-		logrus.Fatal(err)
-	}
+	resultChan := make(chan error)
 
-	volAgent := NewVolumeAgent(healthCheckBaseDir, socket, hostUuid, healthCheckInterval, cattleClient, driver)
+	go func(rc chan<- error) {
+		cmdArgs := buildConvoyCmdArgs(c, socket)
+		cmd := exec.Command("convoy", cmdArgs...)
+		logrus.Infof("Launching convoy with args: %s", cmdArgs)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		logrus.Infof("convoy exited with error: %v", err)
+		rc <- err
+	}(resultChan)
 
-	if err := volAgent.Run(controlChan); err != nil {
-		logrus.Fatal(err)
+	go func(rc chan<- error) {
+		controlChan := make(chan bool, 1)
+		cattleClient, err := cattle.NewCattleClient(cattleUrl, cattleAccessKey, cattleSecretKey)
+		if err != nil {
+			rc <- fmt.Errorf("Error getting cattle client: %v", err)
+		}
+		volAgent := NewVolumeAgent(healthCheckBaseDir, socket, hostUuid, healthCheckInterval, cattleClient, driver)
+		err = volAgent.Run(controlChan)
+		logrus.Infof("volume-agent exited with error: %v", err)
+		rc <- err
+	}(resultChan)
+
+	<-resultChan
+	logrus.Info("Exiting.")
+}
+
+func buildConvoyCmdArgs(c *cli.Context, socket string) []string {
+	convoyCmd := []string{fmt.Sprintf(flagFmt, "socket", socket), "daemon"}
+	for flagName, flagType := range convoyFlags {
+		if !c.IsSet(flagName) {
+			continue
+		}
+		f := c.Generic(flagName)
+		flagName = flagName[len(convoyFlagNamePrefix):]
+		logrus.Infof("Got: %s %v", flagName, f)
+		switch flagType {
+		case "string":
+			fallthrough
+		case "bool":
+			fl := f.(flag.Getter)
+			convoyCmd = append(convoyCmd, fmt.Sprintf(flagFmt, flagName, fl.String()))
+		case "stringslice":
+			fl := f.(*cli.StringSlice)
+			for _, val := range fl.Value() {
+				convoyCmd = append(convoyCmd, fmt.Sprintf(flagFmt, flagName, val))
+			}
+		}
 	}
+	return convoyCmd
 }
